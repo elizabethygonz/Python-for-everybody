fc428912d847088abcfc8dbea99f6de25c024db1
Elizabeth Yumiguano <122760374+elizabethygonz@users.noreply.github.com>
2023-04-19 12:50:24 -0500

    Update Week_2_Assigment_1.py

--- Course_1_Getting_Started_with_Python/Week_2_Assigment_1.py
+++ Course_1_Getting_Started_with_Python/Week_2_Assigment_1.py
@@ -1,6 +1,6 @@
-#Here we make our first code un python
+#Here we make our first code in python
 print("hello world")
 #For the assigment you should write a different sentence
-#You should be careful of  quotation marks
-#For a string is  recommendable to use double-quotes since they are likely to contain single-quote characters as part of the string itself
+#You should be careful of quotation marks
+#For a string is recommendable to use double-quotes since they are likely to contain single-quote characters as part of the string itself
 print("Hello, my name is Elizabeth. I'm from Ecuador")
