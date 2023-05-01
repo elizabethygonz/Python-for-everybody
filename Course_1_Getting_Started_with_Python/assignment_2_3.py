58bda1464d0d2dabd400e09dd226a03a95134546
Elizabeth Yumiguano <122760374+elizabethygonz@users.noreply.github.com>
2023-05-01 18:46:31 -0500

    Update assignment_2_3.py

--- Course_1_Getting_Started_with_Python/assignment_2_3.py
+++ Course_1_Getting_Started_with_Python/assignment_2_3.py
@@ -1,10 +1,8 @@
-# This first line is provided for you
+# Inputs by user
 hrs = float(input("Enter Hours:"))
 rate = float(input("Enter Rate:"))
-thepay = rate*hrs
-thepaystr = str(thepay)
-print("Pay: " + thepaystr)
-
-#or
-#thepay = float(hrs)*float(rate)
-#print("Pay:",thepay)
+#Pay calculation
+pay = rate*hrs
+#Pay value to string
+paystr = str(pay)
+print("Pay: " + paystr)
