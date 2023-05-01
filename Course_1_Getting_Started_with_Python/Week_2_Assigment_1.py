2bf313eeffc927e51bcb35f2143ffb9f771eef1f
Elizabeth Yumiguano <122760374+elizabethygonz@users.noreply.github.com>
2023-05-01 18:47:03 -0500

    Delete Week_2_Assigment_1.py

--- Course_1_Getting_Started_with_Python/Week_2_Assigment_1.py
+++
@@ -1,5 +0,0 @@
-#Here we make our first code in python
-print("hello world")
-#For the assigment you should write a different sentence
-print("Hello, my name is Elizabeth. I'm from Ecuador")
-#For a string is recommendable to use double-quotes since they are likely to contain single-quote characters as part of the string itself
